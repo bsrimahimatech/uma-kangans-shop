a6b4a1e36e9b8184c5dc14f39b715a5ca68c461c
bsrimahimatech <srimahimachowdary@gmail.com>
2026-06-23 17:17:40 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://agfmijteenwwyslxisvu.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnZm1panRlZW53d3lzbHhpc3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDM5OTEsImV4cCI6MjA5Nzc3OTk5MX0.986CyUwLa3msTvKWKb5WJ5S1SXGkWBOM69mjrty10tI";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
